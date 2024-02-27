2a423f6c90f69c0ad701208b180c1db67f0cab0d
Igor-Mayer-Soares <igor@Igor>
2024-02-27 20:12:32 -0300

    Started C++ Simulator

---
+++ AVL/Analysis/aero.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+
+double alpha, beta;
+
+double CDde = 2.464E-07*pow(alpha,2) - 2.321E-05*alpha - 0.000705;
+double CLa = -0.001727*pow(alpha,2) - 0.01255*alpha + 4.452;
+double CLde = -3.856E-06*pow(alpha,2) - 1.619E-06*alpha + 0.01008;
+double CLq = -0.000975*pow(alpha,2) - 0.003977*alpha + 8.073;
+double Cma = 0.0005436*pow(alpha,2) + 8.967E-05*alpha - 0.9113;
+double Cmde = 6.589E-06*pow(alpha,2) - 1.886E-07*alpha - 0.02209;
+double Cmq = 0.001331*pow(alpha,2) - 9.784E-06*alpha - 8.782;
+double CDda = 9.52E-06*beta + 1.747E-21;
+double CDdr = 4.656E-05*beta + 1.398E-20;
+double CYb = -0.3346;
+double CYda = 0.0002835;
+double CYdr = -0.003841;
+double CYp = 0.09342;
+double CYr = 0.296;
+double Clb = -0.04695;
+double Clda = -0.003737;
+double Cldr = -0.000162;
+double Clp = -0.3939;
+double Clr = 0.1068;
+double Cnb = 0.4726;
+double Cnda = 0.0001085;
+double Cndr = 0.001799;
+double Cnp = -0.02878;
+double Cnr = -0.1353;
